9dcd5aed82ddec52359607ffa0130adcc4fe7d8f
ms78600995211 <102867146+ms78600995211@users.noreply.github.com>
2022-04-20 11:31:17 -0700

    Add files via upload

---
+++ SmartVirtualKeyApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartvirtualkeys;
+
+import org.example.smartvirtualkey.screen.Welcomescreen;
+
+public class SmartVirtualKeyApplication {
+public static void main(String[] args) {
+    	
+    	Welcomescreen welcome = new Welcomescreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
